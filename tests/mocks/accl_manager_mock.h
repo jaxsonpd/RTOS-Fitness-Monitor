ccfadf4a8d0878f705f8e1924ae7f4bdf56ca016
Isaac Cone <ico29@uclive.ac.nz>
2024-07-30 13:43:04 +1200

    setup files for testing accl.

---
+++ tests/mocks/accl_manager_mock.h
@@ -0,0 +1,20 @@
+#include "fff.h"
+#include "accl_manager.h"
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef FFF_MOCK_IMPL
+    #define VOID_FUNC FAKE_VOID_FUNC
+    #define VALUE_FUNC FAKE_VALUE_FUNC
+#else
+    #define VOID_FUNC DECLARE_FAKE_VOID_FUNC
+    #define VALUE_FUNC DECLARE_FAKE_VALUE_FUNC
+#endif
+
+#define FFF_I2CDRIVER_FAKES_LIST(FUNC) \
+    FUNC(I2CGenTransmit)               \
+    FUNC(I2CGenIsNotIdle)              \
+
+VALUE_FUNC(char, I2CGenTransmit, char*, int32_t, bool, char);
+VALUE_FUNC(bool, I2CGenIsNotIdle);
---
+++ tests/test_accl_manager.c
@@ -0,0 +1,47 @@
+#include "unity.h"
+#include "accl_manager.h"
+
+#include "fff.h"
+DEFINE_FFF_GLOBALS;
+#define FFF_MOCK_IMPL // Includes mock implementations
+
+#include "tiva_mocks/sysctl_mock.h"
+#include "i2c_driver_mock.h"
+
+/* Helper functions */ 
+void reset_fff(void)
+{
+    FFF_I2CDRIVER_FAKES_LIST(RESET_FAKE);
+    FFF_SYSCTL_FAKES_LIST(RESET_FAKE);
+    FFF_RESET_HISTORY();
+}
+
+// Checks that func is called once with args arg0, arg1
+#define assert_func_called_with_args(func, arg0, arg1) \
+    TEST_ASSERT_EQUAL(1, func.call_count);   \
+    TEST_ASSERT_EQUAL(arg0, func.arg0_val);  \
+    TEST_ASSERT_EQUAL(arg1, func.arg1_val);  \
+
+
+/* Unity setup and teardown */
+void setUp(void)
+{
+    reset_fff();
+}
+
+void tearDown(void)
+{
+    
+}
+
+/* Test cases */
+void test_accl_init_initialises_peripherals(void)
+{
+    // Arrange
+
+    // Act
+    acclInit();
+
+    // Assert
+    TEST_ASSERT_EQUAL(1,SysCtlPeripheralEnable_fake.call_count);
+}
