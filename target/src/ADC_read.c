fa10a264190edcf9334d097ef3190fbe9b6136f2
Jack Duignan <jdu80@uclive.ac.nz>
2024-08-05 11:12:43 +1200

    Fix test errors due to wait for ready in adc

--- target/src/ADC_read.c
+++ target/src/ADC_read.c
@@ -24,7 +24,7 @@
 //*****************************************************************************
 // Global variables
 //*****************************************************************************
-static CircBuf_t ADC_inBuffer;		// Buffer of size BUF_SIZE integers (sample values)
+static CircBuf_t ADC_inBuffer; // Buffer of size BUF_SIZE integers (sample values)
 
 //*****************************************************************************
 //
@@ -37,7 +37,7 @@ void pollADC(void)
     // Initiate a conversion
     //
     ADCProcessorTrigger(ADC0_BASE, 3);
-//    g_ulSampCnt++;
+    //    g_ulSampCnt++;
 }
 
 //*****************************************************************************
@@ -48,35 +48,37 @@ void pollADC(void)
 //*****************************************************************************
 void ADCIntHandler(void)
 {
-	uint32_t ulValue;
-	
-	// Get the single sample from ADC0.  ADC_BASE is defined in
-	// inc/hw_memmap.h
-	ADCSequenceDataGet(ADC0_BASE, 3, &ulValue);
-	//
-	// Place it in the circular buffer (advancing write index)
-	CircBuf_write (&ADC_inBuffer, ulValue);
-	//
-	// Clean up, clearing the interrupt
-	ADCIntClear(ADC0_BASE, 3);                          
+    uint32_t ulValue;
+
+    // Get the single sample from ADC0.  ADC_BASE is defined in
+    // inc/hw_memmap.h
+    ADCSequenceDataGet(ADC0_BASE, 3, &ulValue);
+    //
+    // Place it in the circular buffer (advancing write index)
+    CircBuf_write(&ADC_inBuffer, ulValue);
+    //
+    // Clean up, clearing the interrupt
+    ADCIntClear(ADC0_BASE, 3);
 }
 
 //*****************************************************************************
 // Initialisation functions for the ADC
 //*****************************************************************************
 
-void initADC (void)
+void initADC(void)
 {
     //
-    CircBuf_init (&ADC_inBuffer, ADC_BUF_SIZE);
+    CircBuf_init(&ADC_inBuffer, ADC_BUF_SIZE);
     // The ADC0 peripheral must be enabled for configuration and use.
     SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+
+    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_ADC0));
     
     // Enable sample sequence 3 with a processor signal trigger.  Sequence 3
     // will do a single sample when the processor sends a signal to start the
     // conversion.
     ADCSequenceConfigure(ADC0_BASE, 3, ADC_TRIGGER_PROCESSOR, 0);
-  
+
     //
     // Configure step 0 on sequence 3.  Sample channel 0 (ADC_CTL_CH0) in
     // single-ended mode (default) and configure the interrupt flag
@@ -86,36 +88,35 @@ void initADC (void)
     // sequence 0 has 8 programmable steps.  Since we are only doing a single
     // conversion using sequence 3 we will only configure step 0.  For more
     // on the ADC sequences and steps, refer to the LM3S1968 datasheet.
-    ADCSequenceStepConfigure(ADC0_BASE, 3, 0, ADC_CTL_CH0 | ADC_CTL_IE |
-                             ADC_CTL_END);    
-                             
+    ADCSequenceStepConfigure(ADC0_BASE, 3, 0, ADC_CTL_CH0 | ADC_CTL_IE | ADC_CTL_END);
+
     //
     // Since sample sequence 3 is now configured, it must be enabled.
     ADCSequenceEnable(ADC0_BASE, 3);
-  
+
     //
     // Register the interrupt handler
-    ADCIntRegister (ADC0_BASE, 3, ADCIntHandler);
-  
+    ADCIntRegister(ADC0_BASE, 3, ADCIntHandler);
+
     //
     // Enable interrupts for ADC0 sequence 3 (clears any outstanding interrupts)
     ADCIntEnable(ADC0_BASE, 3);
-
 }
 
-uint32_t readADC() {
+uint32_t readADC()
+{
     int32_t value;
     uint32_t sum = 0;
     uint32_t i = 0;
-    
+
     // Read the current data from the buffer
-    while ((value = CircBuf_read(&ADC_inBuffer)) != BUF_EMPTY_VAL && 
-        i < ADC_BUF_SIZE) {
-        
+    while ((value = CircBuf_read(&ADC_inBuffer)) != BUF_EMPTY_VAL &&
+           i < ADC_BUF_SIZE)
+    {
+
         sum += value;
-        i ++;
+        i++;
     }
 
     return sum / i;
 }
-
