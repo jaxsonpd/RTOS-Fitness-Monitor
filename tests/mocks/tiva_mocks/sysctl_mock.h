c4cf7d6c9e1e517bf0f384b5752547fdfa7decb1
Isaac Cone <ico29@uclive.ac.nz>
2024-08-05 16:06:55 +1200

    resolved test build error

--- tests/mocks/tiva_mocks/sysctl_mock.h
+++ tests/mocks/tiva_mocks/sysctl_mock.h
@@ -13,8 +13,8 @@
 #define FFF_SYSCTL_FAKES_LIST(FUNC)     \
     FUNC(SysCtlPeripheralEnable)        \
     FUNC(SysCtlClockGet)                \
-    FUNC(SysCtlPeripheralReset)         \\
-    FUNC(SysCtlPeripheralReady)    
+    FUNC(SysCtlPeripheralReset)         \
+    FUNC(SysCtlPeripheralReady)         \
 
 VOID_FUNC(SysCtlPeripheralEnable, uint32_t);
 VALUE_FUNC(uint32_t, SysCtlClockGet);
