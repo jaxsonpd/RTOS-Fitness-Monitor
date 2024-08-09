9af9b6e764548dd615fd532acb0b1640ff473dc4
Isaac Cone <ico29@uclive.ac.nz>
2024-08-10 01:50:29 +1200

    added comments to adc_hal header

--- target/src/adc_hal.h
+++ target/src/adc_hal.h
@@ -17,10 +17,23 @@ typedef enum {
 
 typedef void (*callback_t)(uint32_t);
 
+/**
+ * @brief check interrupt flags, handle raised interrupts from callback, 
+ * clear flags. 
+ */
 void adc_hal_isr(void);
 
-void adc_hal_register(uint8_t, callback_t);
+/**
+ * @brief enable adc peripheral and register callback function to instance
+ * @param id adc peripheral id
+ * @param callback callback function pointer to handle uint32_t reading
+ */
+void adc_hal_register(uint8_t id, callback_t callback);
 
-void adc_hal_conversion(uint8_t);
+/**
+ * @brief trigger adc peripheral reading to get data
+ * @param id adc peripheral id
+ */
+void adc_hal_conversion(uint8_t id);
 
 #endif // ADC_HAL_
