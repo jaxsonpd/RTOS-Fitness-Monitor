50fc4f0dd278ec2b20152ffe7094be6ebb5b1091
Jack Duignan <jdu80@uclive.ac.nz>
2024-08-12 11:54:42 +1200

    Display tests working

--- tests/test_display.c
+++ tests/test_display.c
@@ -16,11 +16,14 @@ DEFINE_FFF_GLOBALS;
 #define FFF_MOCK_IMPL // Includes mock implementations
 
 #include "OrbitOLEDInterface_mock.h"
+#include "tiva_mocks/ustdlib_mock.h"
 
 #include "display.h"
 
 void reset_fff(void) {
     FFF_ORBIT_OLED_INPUT_FAKES_LIST(RESET_FAKE);
+    FFF_RESET_HISTORY();
+    FFF_USTDLIB_FAKES_LIST(RESET_FAKE);
     FFF_RESET_HISTORY();
 }
 
@@ -36,4 +39,24 @@ void test_display_init_calls_oled(void) {
     display_init();
 
     TEST_ASSERT_EQUAL(1, OLEDInitialise_fake.call_count);
+}
+
+void test_disply_init_successful(void) {
+    TEST_ASSERT(display_init());
+}
+
+void test_display_line_calls_OLED_draw(void) {
+    display_line("Tests", 0, ALIGN_LEFT);
+
+    TEST_ASSERT_EQUAL(1, OLEDStringDraw_fake.call_count);
+}
+
+void test_display_line_correct_row(void) {
+    display_line("Tests", 0, ALIGN_LEFT);
+
+    TEST_ASSERT_EQUAL(0, OLEDStringDraw_fake.arg2_val);
+}
+
+void test_display_line_algins_left(void) {
+    TEST_IGNORE();
 }
