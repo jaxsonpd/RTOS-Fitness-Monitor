604a0a32f384b89a51805494069aa559593f855c
Jack Duignan <jdu80@uclive.ac.nz>
2024-08-13 11:37:00 +1200

    Goal check and reset working

--- target/src/display_manager.c
+++ target/src/display_manager.c
@@ -1,8 +1,8 @@
-/** 
+/**
  * @file display_manager.c
  * @author Jack Duignan (Jdu80@uclive.ac.nz)
  * @date 2024-08-11
- * @brief Definitions for the display manager thread which write information to 
+ * @brief Definitions for the display manager thread which write information to
  * the OLED screen.
  */
 
@@ -24,7 +24,8 @@
 
 #include "display_manager.h"
 
-#define LONG_PRESS_CYCLES 20
+#define LONG_PRESS_TIME 1
+#define FLASH_TIME 2
 
 #define KM_TO_MILES 62/100 // Multiply by 0.6215 to convert, this should be good enough
 #define MS_TO_KMH 36/10
@@ -33,23 +34,64 @@
 #define DEBUG_STEP_INCREMENT 100
 #define DEBUG_STEP_DECREMENT 100
 
-typedef enum
-{
+typedef enum {
     DISPLAY_STEPS = 0,
     DISPLAY_DISTANCE,
     DISPLAY_SET_GOAL,
     DISPLAY_NUM_STATES, // Automatically enumerates to the number of display states there can be
+    DISPLAY_FLASH_RESET,
+    DISPLAY_FLASH_GOAL_REACHED
 } displayMode_t;
 
-void display_update_state(displayMode_t *display_mode, inputCommMsg_t msg);
-void display_update(displayMode_t display_mode);
+void display_update_state(displayMode_t* display_mode, inputCommMsg_t msg);
+void display_update(displayMode_t* display_mode);
 bool display_manager_init(void);
+
+void display_set_goal(void);
+void display_steps(void);
+void display_distance(void);
+void display_reset_screen(void);
+void display_goal_reached(void);
+
+/** 
+ * @brief Update the step counter and check for workout start
+ * 
+ * @return true if the workout is started
+ */
+bool update_steps(void) {
+    uint32_t steps = display_info_get_steps();
+    uint32_t new_steps = step_counter_get();
+    display_info_set_steps(steps + new_steps);
+
+    if (display_info_get_start() == 0 && new_steps > steps) {
+        display_info_set_start(display_info_get_time());
+        return true;
+    }
+
+    return false;    
+}
+
+/** 
+ * @brief Check reached goal
+ * 
+ * @return true if goal has been reached for the first time
+ */
+bool check_goal_reached() {
+    static uint32_t last_goal = 0;
+
+    if (display_info_get_steps() >= display_info_get_goal() && last_goal != display_info_get_goal()) {
+        last_goal = display_info_get_goal();
+        return true;
+    }
+
+    return false;
+}
 
 void display_manager_thread(void* rtos_param) {
     displayMode_t display_mode = DISPLAY_STEPS;
 
     display_manager_init();
-    
+
 
     for (;;) {
         uint8_t num_tries = 0;
@@ -61,29 +103,27 @@ void display_manager_thread(void* rtos_param) {
 
             num_tries++;
         }
+        
+        update_steps();
 
-        uint32_t steps = display_info_get_steps();
-        uint32_t new_steps = step_counter_get();
-        if (display_info_get_start() == 0 && new_steps > steps) {
-            display_info_set_start(display_info_get_ticks());
+        if (check_goal_reached()) {
+            display_mode = DISPLAY_FLASH_GOAL_REACHED;
         }
 
-        display_info_set_steps(steps + new_steps);
-        
-        display_update(display_mode);
+        display_update(&display_mode);
 
         for (uint8_t i = 0; i < NUM_MSGS; i++) {
             display_info_set_input_flag(i, 0);
         }
 
-        vTaskDelay(1000/5);
+        vTaskDelay(1000 / 5);
     }
 }
 
 /**
  * @brief Initialise the display manager thread
- * 
- * @return true if successful 
+ *
+ * @return true if successful
  */
 bool display_manager_init(void) {
     return display_init();
@@ -93,10 +133,12 @@ bool display_manager_init(void) {
 /**
  * @brief Update the global device state based on the message
  * per screen functions are handled in the respective screen module
- * itself 
+ * itself
  * @param msg the comms message
  */
-void display_update_state(displayMode_t *display_mode, inputCommMsg_t msg) {
+void display_update_state(displayMode_t* display_mode, inputCommMsg_t msg) {
+    static uint32_t down_button_p_time = 0;
+
     switch (msg) {
     case (MSG_SCREEN_LEFT):
         *display_mode = (*display_mode + 1) % DISPLAY_NUM_STATES; // flicker when pressing button
@@ -133,13 +175,21 @@ void display_update_state(displayMode_t *display_mode, inputCommMsg_t msg) {
         break;
 
     case (MSG_DOWN_BUTTON_P):
+        down_button_p_time = display_info_get_time();
         if (display_info_get_debug()) {
             if (display_info_get_steps() >= DEBUG_STEP_DECREMENT) {
                 display_info_set_steps(display_info_get_steps() - DEBUG_STEP_INCREMENT);
             } else {
                 display_info_set_steps(0);
             }
-        } 
+        }
+        break;
+
+    case (MSG_DOWN_BUTTON_R):
+        if (display_info_get_time() - down_button_p_time > LONG_PRESS_TIME) {
+            *display_mode = DISPLAY_FLASH_RESET;
+            display_info_set_steps(0); 
+        }
         break;
 
     default:
@@ -150,9 +200,59 @@ void display_update_state(displayMode_t *display_mode, inputCommMsg_t msg) {
 
 }
 
-/** 
+/**
+ * @brief Update the display
+ *
+ */
+void display_update(displayMode_t* display_mode) {
+    static bool first_time_flash = true;
+    static uint32_t flash_start_time = 0;
+
+    switch (*display_mode) {
+    case DISPLAY_STEPS:
+        display_steps();
+        break;
+    case DISPLAY_DISTANCE:
+        display_distance();
+        break;
+    case DISPLAY_SET_GOAL:
+        display_set_goal();
+        break;
+    case DISPLAY_FLASH_RESET:
+        if (first_time_flash) {
+            flash_start_time = display_info_get_time();
+            first_time_flash = false;
+        } 
+        
+        if (display_info_get_time() - flash_start_time > FLASH_TIME) {
+            *display_mode = DISPLAY_STEPS;
+            first_time_flash = true;
+        }
+
+        display_reset_screen();
+        break;
+    case DISPLAY_FLASH_GOAL_REACHED:
+        if (first_time_flash) {
+            flash_start_time = display_info_get_time();
+            first_time_flash = false;
+        } 
+        
+        if (display_info_get_time() - flash_start_time > FLASH_TIME) {
+            *display_mode = DISPLAY_SET_GOAL;
+            first_time_flash = true;
+        }
+        display_goal_reached();
+        break;
+
+    default:
+
+        break;
+    }
+}
+
+/**
  * @brief Display the screen showing the steps count
- * 
+ *
  */
 void display_steps(void) {
     display_line("", 0, ALIGN_CENTRE); // Clear the top line
@@ -164,21 +264,21 @@ void display_steps(void) {
     uint32_t workout_time = 0;
     uint32_t workout_start_time = display_info_get_start();
     if (workout_start_time != 0) {
-        workout_time = display_info_get_ticks() - workout_start_time;
+        workout_time = display_info_get_time() - workout_start_time;
     }
 
     display_time("Time:", workout_time, 2, ALIGN_CENTRE);
 }
 
-/** 
+/**
  * @brief Display the screen showing the distance traveled
- * 
+ *
  */
 void display_distance(void) {
     uint32_t workout_time = 0;
     uint32_t workout_start_time = display_info_get_start();
     if (workout_start_time != 0) {
-        workout_time = display_info_get_ticks() - workout_start_time;
+        workout_time = display_info_get_time() - workout_start_time;
     }
 
     display_time("Time:", workout_time, 1, ALIGN_CENTRE);
@@ -212,7 +312,7 @@ void display_set_goal(void) {
         display_info_set_goal(new_goal);
         // g_display_mode = DISPLAY_STEPS;
     }
-                    
+
     display_line("Set goal:", 0, ALIGN_CENTRE);
     display_value("Current:", "", display_info_get_goal(), 2, ALIGN_CENTRE, false);
 
@@ -233,38 +333,26 @@ void display_set_goal(void) {
     display_line(toDraw, 1, ALIGN_CENTRE);
 }
 
-
-/** 
- * @brief Update the display
- * 
+/**
+ * @brief Display the reset message
+ *
  */
-void display_update(displayMode_t display_mode) {
-    // Check for flash message override
-    // if (deviceState.flashTicksLeft != 0) {
-    //     char* emptyLine = "                ";
-    //     OLEDStringDraw(emptyLine, 0, 0);
-    //     display_line(deviceState.flashMessage, 1, ALIGN_CENTRE);
-    //     OLEDStringDraw(emptyLine, 0, 2);
-    //     OLEDStringDraw(emptyLine, 0, 3);
-    //     return;
-    // }
+void display_reset_screen(void) {
+    char* emptyLine = "                ";
+    display_line(emptyLine, 0, ALIGN_LEFT);
+    display_line("RESET", 1, ALIGN_CENTRE);
+    display_line(emptyLine, 2, ALIGN_LEFT);
+    display_line(emptyLine, 3, ALIGN_LEFT);
+}
 
-
-    // uint32_t mTravelled = 0; // TODO: If I put this inside the case statement it won't compile. Work out why!
-
-    switch (display_mode) {
-    case DISPLAY_STEPS:
-        display_steps();
-        break;
-    case DISPLAY_DISTANCE:
-        display_distance();        
-        break;
-    case DISPLAY_SET_GOAL:
-        display_set_goal();
-        break;
-
-    default:
-
-        break;
-    }
+/**
+ * @brief Display the goal reached message
+ *
+ */
+void display_goal_reached(void) {
+    char* emptyLine = "                ";
+    display_line(emptyLine, 0, ALIGN_LEFT);
+    display_line("GOAL REACHED!", 1, ALIGN_CENTRE);
+    display_line(emptyLine, 2, ALIGN_LEFT);
+    display_line(emptyLine, 3, ALIGN_LEFT);
 }
