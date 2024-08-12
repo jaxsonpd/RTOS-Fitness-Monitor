a1c57452d26824bece1a6fca7087c3916ee9782a
Isaac Cone <ico29@uclive.ac.nz>
2024-08-12 12:35:08 +1200

    added tests.

---
+++ tests/mocks/step_counter_mock.h
@@ -0,0 +1,18 @@
+#include "fff.h"
+#include "step_counter.h"
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#ifdef FFF_MOCK_IMPL
+#define VOID_FUNC FAKE_VOID_FUNC
+#define VALUE_FUNC FAKE_VALUE_FUNC
+#else
+#define VOID_FUNC DECLARE_FAKE_VOID_FUNC
+#define VALUE_FUNC DECLARE_FAKE_VALUE_FUNC
+#endif
+
+#define FFF_STEP_COUNTER_FAKES_LIST(FUNC) \
+    FUNC(detect_step)               \
+
+VALUE_FUNC(bool, detect_step, vector3_t, uint32_t*);
